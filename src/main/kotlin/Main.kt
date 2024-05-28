0db43fd68c68e2f1a4394ba696b1a39416dfea10
Rafael Beleza <rafaelbeleza711@gmail.com>
2024-05-28 17:12:51 +0100

    t

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,50 +7,47 @@ import org.apache.pdfbox.pdmodel.font.PDCIDFont
 import org.apache.pdfbox.pdmodel.font.PDFont
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.font.Standard14Fonts
+import org.apache.pdfbox.text.PDFTextStripper
 import java.awt.Font
 import java.io.File
-
-
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 
 fun main() {
-    //Loading an existing document
-    val file = File("src/main/teste.pdf")
-    val document: PDDocument = Loader.loadPDF(file)
+    // PDF file
+    var list: MutableList<Int> = mutableListOf()
+    val fileName = File("src/main/BPI.pdf")
+    val doc: PDDocument = Loader.loadPDF(fileName)
+    val stripper = PDFTextStripper()
+    val text = stripper.getText(doc)
+
+    // StringBuilder para guardar o texto extraido
+    val sb = StringBuilder()
 
 
-    //Retrieving the pages of the document
-    val page = document.getPage(0)
-    val contentStream = PDPageContentStream(document, page)
-
-    //Begin the Content stream
-    contentStream.beginText()
-
-    contentStream.setFont(PDType1Font(Standard14Fonts.FontName.HELVETICA),12F);
-
-    //Setting the position for the line
-    contentStream.newLineAtOffset(25F, 5000F)
-
-    val text = "Reis"
+    // adicionar o text ao StringBuilder do PDF
+    sb.append(stripper.getText(doc))
 
 
-    //Adding text in the form of string
-    contentStream.showText(text)
-
-    //Ending the content stream
-    contentStream.endText()
-
-    println("Content added")
+    // Regex-> Vai defenir o padrao de que estamos a procura neste caso uma data dd-MM-yyyy
+    val p: Pattern= Pattern.compile("\\d{2}-\\d{2}-\\d{4}")
 
 
-    //Closing the content stream
-    contentStream.close()
+    // Encontra o primeiro index do que estamos a procura
+    val m: Matcher = p.matcher(sb)
+    while (m.find()) {
+       //adiciona a uma lista todos as posicoes inicias de uma data encontradas num documento
+        list.add(m.start())
+    }
+    // PDF comeca com uma data nao importante por isso temos que remover primeira localizacao de uma data
+    list.removeFirst()
 
 
-    //Saving the document
-    document.save(File("src/main/teste.pdf"))
-
-
-    //Closing the document
-    document.close()
+    if (doc != null) {
+        doc.close()
+    }
+    for(i in list){
+        println(text.substring(i,))
+    }
 }
--- src/main/kotlin/Read.kt
+++ src/main/kotlin/Read.kt
@@ -1,13 +1,13 @@
 package org.example
 
+import org.apache.pdfbox.Loader
 import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.text.PDFTextStripper
 import java.io.File
+import java.io.IOException
+import java.util.regex.Pattern
+
 
 class Read {
 
-    private val file = File("src/main/teste.pdf")
-
-    fun readTxt(){
-
-    }
 }
